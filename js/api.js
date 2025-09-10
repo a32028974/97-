02a217b94581ca74512f2839d052cbefc482f2b3
a32028974 <a32028974@gmail.com>
2025-09-09 21:54:18 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // js/api.js
 // 1) ENDPOINT GENERAL (DNI, ARMAZÓN, HISTORIAL, setPdf)
-export const API_URL  = "https://script.google.com/macros/s/AKfycbw9hmr8FjoB4Q2z_SF8QnVjMqMBx2pieZIEtLj9QrZwut1rOkf4L7Ac8TxOMYMD9Jz2UQ/exec";
+export const API_URL  = "https://script.google.com/macros/s/AKfycbzLhu4WEkyN0VlTWSxVPz2IE8aKggZWYTsOht1iB7c-d3uAe69cmBz6gwGVD--tJl1PMA/exec";
 
 // 2) ENDPOINT DE PACK/TELEGRAM (el tuyo que ya funcionaba)
 export const PACK_URL = "https://script.google.com/macros/s/AKfycbyAc51qga-xnN3319jcVmAWwz7NTlNH-Lht3IwRIt8PT0MAy_ZKpcGJiohQZIFPfIONsA/exec";
